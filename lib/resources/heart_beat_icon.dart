94bbe2a68af6292c5aa979f9a426d685e9af2a16
Kouki Badr <badreddinekouki1998@gmail.com>
2021-06-26 05:54:28 +0100

    fix #7: add code documentation

--- lib/resources/heart_beat_icon.dart
+++ lib/resources/heart_beat_icon.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
+///Render a heartbeat animated icon
+///
 class HeartBeatIcon extends StatefulWidget {
+  ///The icon that will be animated
   final IconData icon;
+
+  ///The icon color
+  ///
   final Color color;
+
+  ///The default icon size
+  ///
   final double size;
 
-  const HeartBeatIcon({required this.icon, required this.color, required this.size});
+  const HeartBeatIcon(
+      {required this.icon, required this.color, required this.size});
 
   @override
   _HeartBeatIconState createState() => _HeartBeatIconState();
@@ -19,10 +29,12 @@ class _HeartBeatIconState extends State<HeartBeatIcon>
   @override
   void initState() {
     super.initState();
-    _heartAnimationController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 1200));
-    _heartAnimation = Tween(begin: this.widget.size * 0.7, end: this.widget.size * 0.85).animate(CurvedAnimation(
-        curve: Curves.bounceOut, parent: _heartAnimationController));
+    _heartAnimationController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 1200));
+    _heartAnimation =
+        Tween(begin: this.widget.size * 0.7, end: this.widget.size * 0.85)
+            .animate(CurvedAnimation(
+                curve: Curves.bounceOut, parent: _heartAnimationController));
 
     _heartAnimationController.addStatusListener((AnimationStatus status) {
       if (status == AnimationStatus.completed) {
