078d2e640a574b9238520f696a86abe8087ca7ab
Kouki Badr <badreddinekouki1998@gmail.com>
2021-12-27 11:01:53 +0100

    refactor constant values

--- lib/resources/constants.dart
+++ lib/resources/constants.dart
@@ -1,5 +1,25 @@
+import 'package:flutter/animation.dart';
+import 'package:motion_toast/resources/arrays.dart';
+
 const double defaultRadius = 20;
 const double defaultToastWidth = 350;
 const double motionToastHeight = 80;
 const double motionToastSideBarWidth = 15;
 const double defaultIconSize = 40;
+
+const bool defaultEnableAnimationValue = true;
+const bool defaultDismissableValue = false;
+
+const Duration defaultToastDuration = Duration(
+  seconds: 3,
+);
+
+const Duration defaultAnimationDuration = Duration(
+  milliseconds: 1500,
+);
+
+const ORIENTATION defaultLayoutOrientation = ORIENTATION.ltr;
+const ANIMATION defaultAnimationValue = ANIMATION.fromBottom;
+const Cubic defaultCurveValue = Curves.ease;
+const MOTION_TOAST_POSITION defaultPositionValue = MOTION_TOAST_POSITION.bottom;
+const BACKGROUND_TYPE defaultBackgroundTypeValue = BACKGROUND_TYPE.lighter;
