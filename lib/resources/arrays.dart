c3e9d12d8156fe480b4148ab19d7c61b6aa49872
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2022-04-10 21:22:23 +0530

    refactor: added dartDocs for enums and constants

--- lib/resources/arrays.dart
+++ lib/resources/arrays.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:motion_toast/resources/colors.dart';
 
+/// The colors for the different types of motion toast.
 const motionToastColors = {
   MOTION_TOAST_TYPE.success: successColor,
   MOTION_TOAST_TYPE.error: errorColor,
@@ -10,6 +11,7 @@ const motionToastColors = {
   MOTION_TOAST_TYPE.delete: deleteColor
 };
 
+/// Material Icons for the different types of motion toast.
 const motionToastIconsMD = {
   MOTION_TOAST_TYPE.success: Icons.check_circle_outline,
   MOTION_TOAST_TYPE.error: Icons.error,
@@ -18,6 +20,7 @@ const motionToastIconsMD = {
   MOTION_TOAST_TYPE.delete: Icons.delete
 };
 
+/// Cupertino Icons for the different types of motion toast.
 const motionToastIconsCupertino = {
   MOTION_TOAST_TYPE.success: CupertinoIcons.checkmark_alt_circle,
   MOTION_TOAST_TYPE.error: CupertinoIcons.exclamationmark_circle_fill,
@@ -26,14 +29,20 @@ const motionToastIconsCupertino = {
   MOTION_TOAST_TYPE.delete: CupertinoIcons.delete_simple,
 };
 
+/// Type of motion toast to show.
 enum MOTION_TOAST_TYPE { success, error, warning, info, delete, custom }
 
+/// The design of the toast.
 enum ICON_TYPE { materialDesign, cupertino }
 
+/// Text direction used to determine which direction to draw the toast
 enum ORIENTATION { ltr, rtl }
 
+/// The fade in animation direction of the toast.
 enum ANIMATION { fromBottom, fromLeft, fromRight, fromTop }
 
+/// The position of the toast on the screen.
 enum MOTION_TOAST_POSITION { center, bottom, top }
 
+/// The appearance of the background of the toast.
 enum BACKGROUND_TYPE { transparent, solid, lighter }
