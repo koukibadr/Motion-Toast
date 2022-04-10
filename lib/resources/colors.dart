5d3a16a510614ed6371b8d8e68662885dab15637
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2022-04-10 21:27:32 +0530

    refactor: added dartDocs to color constants

--- lib/resources/colors.dart
+++ lib/resources/colors.dart
@@ -1,7 +1,26 @@
 import 'dart:ui';
 
+/// The default color of the success motion toast.
+///
+/// Equals to `Color(0xff28A745)`.
 const Color successColor = Color(0xff28A745);
+
+/// The default color of the warning motion toast.
+///
+/// Equals to `Color(0xff28A745)`.
 const Color warningColor = Color(0xffFFC107);
+
+/// The default color of the error motion toast.
+///
+/// Equals to `Color(0xffDC3545)`.
 const Color errorColor = Color(0xffDC3545);
+
+/// The default color of the info motion toast.
+///
+/// Equals to `Color(0xFF230DA1)`.
 const Color infoColor = Color(0xFF230DA1);
+
+/// The default color of the delete motion toast.
+///
+/// Equals to `Color(0xFF62AECA)`.
 const Color deleteColor = Color(0xFF62AECA);
