3adf75dc98513fae90d90783d0350abb2bff43a9
koukibadr <badreddinekouki1998@gmail.com>
2024-05-23 22:26:43 +0100

    Remove colors and icons Enums

--- lib/resources/arrays.dart
+++ lib/resources/arrays.dart
@@ -1,34 +1,3 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:motion_toast/resources/colors.dart';
-
-/// The colors for the different types of motion toast.
-const motionToastColors = {
-  MotionToastType.success: successColor,
-  MotionToastType.error: errorColor,
-  MotionToastType.warning: warningColor,
-  MotionToastType.info: infoColor,
-  MotionToastType.delete: deleteColor,
-};
-
-/// Material Icons for the different types of motion toast.
-const motionToastIconsMD = {
-  MotionToastType.success: Icons.check_circle_outline,
-  MotionToastType.error: Icons.error,
-  MotionToastType.warning: Icons.warning,
-  MotionToastType.info: Icons.info,
-  MotionToastType.delete: Icons.delete,
-};
-
-/// Cupertino Icons for the different types of motion toast.
-const motionToastIconsCupertino = {
-  MotionToastType.success: CupertinoIcons.checkmark_alt_circle,
-  MotionToastType.error: CupertinoIcons.exclamationmark_circle_fill,
-  MotionToastType.warning: CupertinoIcons.exclamationmark_triangle_fill,
-  MotionToastType.info: CupertinoIcons.info_circle_fill,
-  MotionToastType.delete: CupertinoIcons.delete_simple,
-};
-
 /// Type of motion toast to show.
 enum MotionToastType { success, error, warning, info, delete, custom }
 
