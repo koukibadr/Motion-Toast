0730c3b38a2b60b730638f2f4d08126a98707854
koukibadr <badreddinekouki1998@gmail.com>
2024-05-25 10:38:23 +0100

    Update motion toast themes colors

--- lib/widgets/motion_toast_background.dart
+++ lib/widgets/motion_toast_background.dart
@@ -1,36 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:motion_toast/resources/arrays.dart';
 
-/// Widget that renders a motion toast background
 class MotionToastBackground extends StatelessWidget {
   final double borderRadius;
   final Color backgroundColor;
   final Widget child;
-  final BackgroundType backgroundType;
   final Color borderColor;
   final bool displayBorder;
   final Brightness brightness;
   final EdgeInsets contentPadding;
+  final double opacity;
 
   const MotionToastBackground({
     Key? key,
     required this.borderRadius,
     required this.backgroundColor,
     required this.child,
-    required this.backgroundType,
     required this.borderColor,
     required this.displayBorder,
     required this.brightness,
     required this.contentPadding,
+    required this.opacity,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: backgroundType == BackgroundType.solid
-            ? backgroundColor
-            : backgroundColor.withOpacity(0.8),//TODO expose this as a parameter
+        color: backgroundColor.withOpacity(opacity),
         borderRadius: BorderRadius.all(
           Radius.circular(borderRadius),
         ),
