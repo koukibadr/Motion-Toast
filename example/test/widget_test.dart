941d559eb043e381855f582d5670d4fe5d34f70f
Badr Kouki <badreddinekouki1998@gmail.com>
2025-05-10 10:41:20 +0100

    test: implement widget test for success motion toast type

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -3,7 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   test('Counter increments smoke test', () async {
     // This is an example test that checks if the counter increments correctly.
-    // TODO - Implement the actual widget test.
     expect(1, 1);
   });
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,11 +4,105 @@
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:motion_toast/motion_toast.dart';
 
-// TODO: write unit tests for motion toast widget
 void main() {
-  test('Counter increments smoke test', () async {
-    expect(true, isTrue);
+  group('Success motion toast testing...', () {
+    testWidgets(
+      'Basic motion toast widget test',
+      (tester) async {
+        var basicMotionToastWidget = MotionToast.success(
+          description: const Text(
+            'Elegant notification description',
+          ),
+        );
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: basicMotionToastWidget,
+              ),
+            ),
+          ),
+        );
+        expect(
+          find.byType(Text),
+          findsOneWidget,
+        );
+        expect(
+          find.byType(Icon),
+          findsOneWidget,
+        );
+      },
+    );
+
+    testWidgets(
+      'Motion toast with title widget test',
+      (tester) async {
+        var basicMotionToastWidget = MotionToast.success(
+          description: const Text(
+            'Elegant notification description',
+          ),
+          title: const Text(
+            'Elegant notification title',
+          ),
+        );
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: basicMotionToastWidget,
+              ),
+            ),
+          ),
+        );
+        expect(
+          find.byType(Text),
+          findsAtLeast(2),
+        );
+        expect(
+          find.byType(Icon),
+          findsOneWidget,
+        );
+      },
+    );
+
+    testWidgets(
+      'Motion toast with title and toast duration widget test',
+      (tester) async {
+        var basicMotionToastWidget = MotionToast.success(
+          description: const Text(
+            'Elegant notification description',
+          ),
+          title: const Text(
+            'Elegant notification title',
+          ),
+          toastDuration: const Duration(seconds: 1),
+        );
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: basicMotionToastWidget,
+              ),
+            ),
+          ),
+        );
+        expect(
+          find.byType(Text),
+          findsAtLeast(2),
+        );
+        expect(
+          find.byType(Icon),
+          findsOneWidget,
+        );
+        expect(
+          find.byType(MotionToast),
+          findsOneWidget,
+        );
+      },
+    );
   });
 }
