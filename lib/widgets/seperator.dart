3336b2d2f3636c612e9bca29fc63c62d81049236
koukibadr <badreddinekouki1998@gmail.com>
2026-08-31 19:50:10 +0300

    config: upgrade flutter version with UI decoupling

--- lib/widgets/seperator.dart
+++ lib/widgets/seperator.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:material_ui/material_ui.dart';
 
 class Seperator extends StatelessWidget {
   // The distance between the contents of the toast.
